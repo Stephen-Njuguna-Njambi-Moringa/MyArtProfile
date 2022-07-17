2d1a8e795c0ea86942332167dd316c6f30dd5907
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-07-17 21:32:17 +0300

    Added a mathematical function to calculate the total production

--- Assets/index.js
+++ Assets/index.js
@@ -1,25 +1,39 @@
 // declaring variables for input fields.
-// let inputVal1 = document.getElementById('input1').value;
-// let inputVal2 = document.getElementById('input2').value;
-// let inputVal3 = document.getElementById('input3').value;
-// let inputVal4 = document.getElementById('input4').value;
-
-
-let reportBtn = document.getElementById('submit-btn');
-let reportSection = document.getElementsByClassName('report-section');
-
-// creating a button click.
-reportBtn.addEventListener('click', function(){ 
     let inputVal1 = document.getElementById('input1').value;
     let inputVal2 = document.getElementById('input2').value;
     let inputVal3 = document.getElementById('input3').value;
     let inputVal4 = document.getElementById('input4').value;
-    document.getElementById('rs').style.display='block';
-    document.getElementById('val1').innerHTML=inputVal1;
-    document.getElementById('val2').innerHTML=inputVal2;
-    document.getElementById('val3').innerHTML=inputVal3;
-    document.getElementById('val4').innerHTML=inputVal4;
+
+
+    let reportBtn = document.getElementById('submit-btn');
+    let reportSection = document.getElementsByClassName('report-section');
+
+   
+
+// creating a button click.
+    reportBtn.addEventListener('click', function(){ 
+        let inputVal1 = document.getElementById('input1').value;
+        let inputVal2 = document.getElementById('input2').value;
+        let inputVal3 = document.getElementById('input3').value;
+        let inputVal4 = document.getElementById('input4').value;
+        
+        document.getElementById('rs').style.display='block';
+        document.getElementById('val1').innerHTML=inputVal1;
+        document.getElementById('val2').innerHTML=inputVal2;
+        document.getElementById('val3').innerHTML=inputVal3;
+        document.getElementById('val4').innerHTML=inputVal4;
+
+// Calculate the total
+        function totalProduction(){
+            var total= parseInt(inputVal1)+ parseInt(inputVal2)+ parseInt(inputVal3)+parseInt(inputVal4);// converted the text input values to numbers using parseint().
+            return `Total Production: ${total} litres per day`;
+        }
+        document.getElementById('total').innerHTML=totalProduction();parseint
+
+    })
     
-})
+   
+
+    console.log(totalProduction());
 
 
